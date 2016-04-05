4f10f9bb4f18cbd67fe7f8e17f678396591e7034
Gaubee <gaubeebangeel@gmail.com>
2016-04-06 00:40:24 +0800

    Signed-off-by: Gaubee <gaubeebangeel@gmail.com>

---
+++ js/index.js
@@ -0,0 +1,57 @@
+var imgs = ["19011030-ea7cca1ce2d34d70aa678b6336d7decc.jpg", "sun.jpg", "snow.jpg"]
+var qs = {}
+location.search.substr(1).split("&").forEach(function(data) {
+	var info = data.split("=");
+	qs[info[0]] = info[1]
+});
+var PSV = new PhotoSphereViewer({
+	// Path to the panorama
+	panorama: imgs[~~qs.img % imgs.length],
+
+	// Container
+	container: document.getElementById("container"),
+
+	// Deactivate the animation
+	time_anim: false,
+
+	// Display the navigation bar
+	navbar: true,
+
+	// Resize the panorama
+	size: {
+		width: '100%',
+		height: '100%'
+	}
+});
+var DTextNode = document.getElementById("d-info");
+var MTextNode = document.getElementById("m-info");
+
+// window.addEventListener("deviceorientation", function(e) {
+// 	// console.log("F:",
+// 	// 	e.absolute,
+// 	// 	e.alpha,
+// 	// 	e.beta,
+// 	// 	e.gamma,
+// 	// 	"--"
+// 	// );
+
+// 	DTextNode.innerHTML = e.alpha + "<br>" +
+// 		e.beta + "<br>" +
+// 		e.gamma + "<br>"
+// }, false);
+
+// window.addEventListener("devicemotion", function(e) {
+// 	// console.log("M:",
+// 	// 	e.acceleration,
+// 	// 	e.accelerationIncludingGravity,
+// 	// 	e.rotationRate,
+// 	// 	e.interval,
+// 	// 	"--"
+// 	// );
+
+
+// 	MTextNode.innerHTML = JSON.stringify(e.acceleration) + "<br>" +
+// 		JSON.stringify(e.accelerationIncludingGravity) + "<br>" +
+// 		JSON.stringify(e.rotationRate) + "<br>" +
+// 		e.interval + "<br>"
+// }, false)
